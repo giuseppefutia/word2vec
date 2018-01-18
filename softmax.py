b3133025307e85eb801d5a0e8e21357e6b55c71f
Giuseppe Futia <giuseppe.futia@gmail.com>
2018-01-18 14:31:03 +0100

    Apply softmax from matrix of shape (m,n) to shape (n,m)

--- softmax.py
+++ softmax.py
@@ -10,23 +10,24 @@ TODO: Implement softmax gradient for vector
 def softmax(Z):
     """
     Arguments:
-    Z -- numpy array of any shape
+    Z -- numpy array of shape (n,m) where n is the number of features while m is the number of samples
+    In other words, each coloumn is a sample of data
 
     Return:
     A -- Output of the softmax function
     cache -- returns Z as well, useful during backpropagation
     """
 
-    # Softmax implementation for matrix. I need to apply the algorithm for each row (axis=1)
+    # Softmax implementation for matrix. I need to apply the algorithm for each coloumn (axis=0)
     if len(Z.shape) > 1:
-        max_matrix = np.max(Z, axis=1)
-        max_matrix = max_matrix.reshape(max_matrix.shape[0], 1)
+        max_matrix = np.max(Z, axis=0)
         stable_Z = Z - max_matrix
         e = np.exp(stable_Z)
-        A = np.divide(e, np.sum(e, axis=1, keepdims=True))
+        A = np.divide(e, np.sum(e, axis=0, keepdims=True))
     # Softmax implementation for vector.
     else:
         vector_max_value = np.max(Z)
+        print(vector_max_value)
         A = (np.exp(Z - vector_max_value)) / sum(np.exp(Z - vector_max_value))
 
     assert A.shape == Z.shape
@@ -37,6 +38,22 @@ def softmax(Z):
 
 
 def softmax_grad(cache):
+    """
+    Arguments:
+    cache -- 'Z' where we store for computing backward propagation efficiently
+
+    Return:
+    dZ -- Jacobian Matrix
+
+    np.array([D1S1,...,D1SN],
+             [D2S1,...,D2SN],
+             ...,
+             [D1SN,...,DNSN])
+
+    Si(1-Sj) i=j
+    -SjSi i different from j
+
+    """
     Z = cache
     s = softmax(Z)[0].reshape(-1, 1)
     dZ = np.diagflat(s) - np.dot(s, s.T)
@@ -47,38 +64,34 @@ def softmax_grad(cache):
 
 
 def test_softmax_and_its_gradient():
-    print("Running basic tests...")
+    print("Running softmax() and softmax_grad() tests...")
 
     test1, cache = softmax(np.array([1,2]))
+    print(test1)
     ans1 = np.array([0.26894142,  0.73105858])
     assert np.allclose(test1, ans1, rtol=1e-05, atol=1e-06)
 
-    test2, cache = softmax(np.array([[1001,1002],[3,4]]))
+    test2, cache = softmax(np.array([[1001,3],[1002,4]]))
+    print(test2)
     ans2 = np.array([
-        [0.26894142, 0.73105858],
-        [0.26894142, 0.73105858]])
+        [0.26894142, 0.26894142],
+        [0.73105858, 0.73105858]])
     assert np.allclose(test2, ans2, rtol=1e-05, atol=1e-06)
-
-    test3, cache = softmax(np.array([[-1001,-1002]]))
-    ans3 = np.array([0.73105858, 0.26894142])
-    assert np.allclose(test3, ans3, rtol=1e-05, atol=1e-06)
 
     #test1_grad = softmax_grad(np.array([1,2]))
     #print(test1_grad)
 
-    test2_grad = softmax_grad(np.array([[1001,1002,1003],[3,4,5]]))
+    test2_grad = softmax_grad(np.array([[1001,3],[1002,4]]))
+    print(test2_grad)
     anstest2_grad = np.array([
-    [ 0.08192507, -0.02203304, -0.05989202, -0.0081055,  -0.02203304, -0.05989202,],
-    [-0.02203304,  0.18483645, -0.1628034,  -0.02203304, -0.05989202, -0.1628034, ],
-    [-0.05989202, -0.1628034,   0.22269543, -0.05989202, -0.1628034,  -0.44254553,],
-    [-0.0081055,  -0.02203304, -0.05989202,  0.08192507, -0.02203304, -0.05989202,],
-    [-0.02203304, -0.05989202, -0.1628034,  -0.02203304,  0.18483645, -0.1628034, ],
-    [-0.05989202, -0.1628034,  -0.44254553, -0.05989202, -0.1628034,   0.22269543,],
-    ])
+        [ 0.19661193,-0.07232949,-0.19661193,-0.19661193],
+        [-0.07232949,0.19661193,-0.19661193,-0.19661193],
+        [-0.19661193,-0.19661193,0.19661193,-0.53444665],
+        [-0.19661193,-0.19661193,-0.53444665,0.19661193]])
 
     assert np.allclose(test2_grad, anstest2_grad, rtol=1e-05, atol=1e-06)
 
-    print("... test OK!")
+    print("... end tests")
 
 if __name__ == "__main__":
     test_softmax_and_its_gradient()
