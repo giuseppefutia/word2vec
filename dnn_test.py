2f0ad160c2ced88384f21f337d8c4bdf105e07fd
Giuseppe Futia <giuseppe.futia@gmail.com>
2018-01-23 11:59:33 +0100

    Add test for the implemented neural network

---
+++ dnn_test.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+
+import time
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+import scipy
+from PIL import Image
+from scipy import ndimage
+from dnn import *
+
+
+def two_layer_model(hyper_parameters, X, Y, layers_dims, num_iterations = 3000, print_cost=False):
+    """
+    Implements a two-layer neural network: LINEAR->RELU->LINEAR->SIGMOID.
+
+    Arguments:
+    hyper_parameters -- hyper parameters for the neural network
+    X -- input data, of shape (n_x, number of examples)
+    Y -- true "label" vector (containing 0 if cat, 1 if non-cat), of shape (1, number of examples)
+    layers_dims -- dimensions of the layers (n_x, n_h, n_y)
+    num_iterations -- number of iterations of the optimization loop
+    print_cost -- If set to True, this will print the cost every 100 iterations
+
+    Returns:
+    parameters -- a dictionary containing W1, W2, b1, and b2
+    """
+
+    np.random.seed(1)
+    grads = {}
+    costs = []                              # to keep track of the cost
+    m = X.shape[1]                          # number of examples
+    (n_x, n_h, n_y) = layers_dims
+
+    # Initialize parameters dictionary, by calling one of the functions you'd previously implemented
+    parameters = initialize_parameters([n_x, n_h, n_y])
+
+    # Get W1, b1, W2 and b2 from the dictionary parameters.
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+
+    # Loop (gradient descent)
+
+    for i in range(0, num_iterations):
+
+        # Forward propagation: LINEAR -> RELU -> LINEAR -> SIGMOID. Inputs: "X, W1, b1". Output: "A1, cache1, A2, cache2".
+        A1, cache1 = linear_activation_forward(X, W1, b1, hyper_parameters["activations"][1])
+        A2, cache2 = linear_activation_forward(A1, W2, b2,  hyper_parameters["activations"][2])
+
+        # Compute cost
+        cost = compute_cost(A2, Y)
+
+        # Initializing backward propagation
+        dA2 = - (np.divide(Y, A2) - np.divide(1 - Y, 1 - A2))
+
+        # Backward propagation. Inputs: "dA2, cache2, cache1". Outputs: "dA1, dW2, db2; also dA0 (not used), dW1, db1".
+        dA1, dW2, db2 = linear_activation_backward(dA2, cache2, hyper_parameters["activations"][2])
+        dA0, dW1, db1 = linear_activation_backward(dA1, cache1, hyper_parameters["activations"][1])
+
+        # Set grads['dWl'] to dW1, grads['db1'] to db1, grads['dW2'] to dW2, grads['db2'] to db2
+        grads['dW1'] = dW1
+        grads['db1'] = db1
+        grads['dW2'] = dW2
+        grads['db2'] = db2
+
+        # Update parameters.
+        parameters = update_parameters(parameters, grads, hyper_parameters["learning_rate"])
+
+        # Retrieve W1, b1, W2, b2 from parameters
+        W1 = parameters["W1"]
+        b1 = parameters["b1"]
+        W2 = parameters["W2"]
+        b2 = parameters["b2"]
+
+        # Print the cost every 100 training example
+        if print_cost and i % 100 == 0:
+            print("Cost after iteration {}: {}".format(i, np.squeeze(cost)))
+        if print_cost and i % 100 == 0:
+            costs.append(cost)
+
+    # plot the cost
+
+    plt.plot(np.squeeze(costs))
+    plt.ylabel('cost')
+    plt.xlabel('iterations (per tens)')
+    plt.title("Learning rate =" + str(hyper_parameters["learning_rate"]))
+    plt.show()
+
+    return parameters
+
+def load_data():
+    train_dataset = h5py.File('datasets/train_catvnoncat.h5', "r")
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
+
+    test_dataset = h5py.File('datasets/test_catvnoncat.h5', "r")
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
+
+    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
+
+    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
+
+def test_two_layer_model():
+    #%matplotlib inline
+    plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
+    plt.rcParams['image.interpolation'] = 'nearest'
+    plt.rcParams['image.cmap'] = 'gray'
+
+    #%load_ext autoreload
+    #%autoreload 2
+
+    np.random.seed(1)
+
+    train_x_orig, train_y, test_x_orig, test_y, classes = load_data()
+
+    # Example of a picture
+    index = 10
+    plt.imshow(train_x_orig[index])
+    print("y = " + str(train_y[0,index]) + ". It's a " + classes[train_y[0,index]].decode("utf-8") +  " picture.")
+
+    # Explore your dataset
+    m_train = train_x_orig.shape[0]
+    num_px = train_x_orig.shape[1]
+    m_test = test_x_orig.shape[0]
+
+    print("Number of training examples: " + str(m_train))
+    print("Number of testing examples: " + str(m_test))
+    print("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
+    print("train_x_orig shape: " + str(train_x_orig.shape))
+    print("train_y shape: " + str(train_y.shape))
+    print("test_x_orig shape: " + str(test_x_orig.shape))
+    print("test_y shape: " + str(test_y.shape))
+
+    # Reshape the training and test examples
+    train_x_flatten = train_x_orig.reshape(train_x_orig.shape[0], -1).T   # The "-1" makes reshape flatten the remaining dimensions
+    test_x_flatten = test_x_orig.reshape(test_x_orig.shape[0], -1).T
+
+    # Standardize data to have feature values between 0 and 1.
+    train_x = train_x_flatten/255.
+    test_x = test_x_flatten/255.
+
+    print("train_x's shape: " + str(train_x.shape))
+    print("test_x's shape: " + str(test_x.shape))
+
+    train_x_shape_expected = (12288, 209)
+    test_x_shape_expected = (12288, 50)
+
+    assert np.allclose(train_x.shape, train_x_shape_expected, rtol=1e-05, atol=1e-06)
+    assert np.allclose(test_x.shape, test_x_shape_expected, rtol=1e-05, atol=1e-06)
+
+    ### Constants for the model
+    n_x = 12288     # num_px * num_px * 3
+    n_h = 7
+    n_y = 1
+    activations = ["relu", "sigmoid"]
+    learning_rate = 0.0075
+    layers_dims = (n_x, n_h, n_y)
+
+    hyper_parameters = initialize_hyper_parameters(activations,
+                                                   learning_rate)
+
+    parameters = two_layer_model(hyper_parameters, train_x, train_y, layers_dims = (n_x, n_h, n_y), num_iterations = 2500, print_cost=True)
+
+    predictions_train = predict(train_x, train_y, parameters)
+
+    predictions_test = predict(test_x, test_y, parameters)
+
+if __name__ == "__main__":
+    test_two_layer_model()
