cb0bc20f6b3a516a26b66329942a6587ddc68232
Giuseppe Futia <giuseppe.futia@gmail.com>
2018-01-31 18:27:33 +0100

    Naive implementation of grandient check for fast word2vec testing

--- gradient_check_naive.py
+++ gradient_check_naive.py
@@ -3,71 +3,60 @@
 import numpy as np
 import random
 
-
-def gradcheck_naive(f, x):
-    """ Gradient check for a function f.
+def gradient_check_naive(f, x):
+    """
+    Gradient check for a function f
 
     Arguments:
-    f -- a function that takes a single argument and outputs the
-         cost and its gradients
-    x -- the point (numpy array) to check the gradient at
+    f -- should be a function that takes a single argument and outputs its gradients
+    x -- is the point (numpy array) to check the gradient at
     """
 
-    rndstate = random.getstate() # It returns a tuple of the internal state of the generator
+    rndstate = random.getstate()
     random.setstate(rndstate)
     fx, grad = f(x) # Evaluate function value at original point
-    h = 1e-4        # Do not change this!
+    h = 1e-4
 
     # Iterate over all indexes in x
-    # More information on the iteration: https://docs.scipy.org/doc/numpy-1.13.0/reference/arrays.nditer.html#arrays-nditer
     it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
     while not it.finished:
         ix = it.multi_index
-
-        # Try modifying x[ix] with h defined above to compute
-        # numerical gradients. Make sure you call random.setstate(rndstate)
-        # before calling f(x) each time. This will make it possible
-        # to test cost functions with built in randomness later.
-
-        # For the gradient checking I have to approximate the derivative of our cost function
-        # REMINDER: f is the function that compute the cost and its gradient
-        # Therefore I have to compute the check gradient exploiting f
-
-        x[ix] = x[ix] + h
+        x[ix] += h
         random.setstate(rndstate)
-        cost_plus, _ = f(x)
-        x[ix] = x[ix] - 2. * h
+        before,_ = f(x)
         random.setstate(rndstate)
-        cost_minus, _ = f(x)
-        x[ix] = x[ix] + h
-        numgrad = (cost_plus - cost_minus) / (2. * h)
+        x[ix] -= 2*h
+        after,_ = f(x)
+        x[ix] += h
+        numgrad = (before - after) / (2*h)
 
         # Compare gradients
-
         reldiff = abs(numgrad - grad[ix]) / max(1, abs(numgrad), abs(grad[ix]))
-
         if reldiff > 1e-5:
             print("Gradient check failed.")
             print("First gradient error found at index %s" % str(ix))
-            print ("Your gradient: %f \t Numerical gradient: %f" % (grad[ix], numgrad))
+            print("Your gradient: %f \t Numerical gradient: %f" % (grad[ix], numgrad))
             return
+
+        print("Check gradient at index %s" % str(ix))
+        print("Gradient by your back prop: %f \t Numerical gradient: %f" % (grad[ix], numgrad))
 
         it.iternext() # Step to next dimension
 
     print("Gradient check passed!")
 
 
-def sanity_check():
+def test_gradient_check_naive():
     """
-    Some basic sanity checks.
+    Some basic sanity checks of the gradient check naive
     """
+    print("\n" + "\033[92m" + "Test gradient_check_naive() ..." + "\033[0m")
     quad = lambda x: (np.sum(x ** 2), x * 2)
-
-    print("Running sanity checks...")
-    gradcheck_naive(quad, np.array(123.456))      # scalar test
-    gradcheck_naive(quad, np.random.randn(3,))    # 1-D test
-    gradcheck_naive(quad, np.random.randn(4,5))   # 2-D test
+    gradient_check_naive(quad, np.array(123.456))      # scalar test
+    gradient_check_naive(quad, np.random.randn(3,))    # 1-D test
+    gradient_check_naive(quad, np.random.randn(4,5))   # 2-D test
+    print("\033[92m" + "... end test" + "\033[0m")
 
 
 if __name__ == "__main__":
-    sanity_check()
+    test_gradient_check_naive()
