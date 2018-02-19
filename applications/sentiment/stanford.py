76722d0d8d5c4859042727023b1c8680c520fae0
Giuseppe Futia <giuseppe.futia@gmail.com>
2018-02-19 12:16:51 +0100

    Add comments

--- applications/sentiment/stanford.py
+++ applications/sentiment/stanford.py
@@ -15,6 +15,15 @@ class StanfordSentiment:
 
 
     def tokens(self):
+        """
+        Get tokens from sentences and extract occurrences
+
+        Returns:
+        tokens -- dict where the key is the token and the value is its id. Example: b'cleaving', 12512
+        tokenfreq -- dict with the frequency of the token. Example: b'cleaving': 1
+        wordcount -- total of words extracted by sentence (considering all occurrences)
+        revtokens -- list of individual tokens
+        """
         if hasattr(self, "_tokens") and self._tokens:
             return self._tokens
 
@@ -48,6 +57,15 @@ class StanfordSentiment:
 
 
     def sentences(self):
+        """
+        Get sentences and count them
+
+        Returns:
+        sentences -- list of sentences. Each element contains the tokens of the sentence. Example:
+        [b'the', b'rock', b'is', b'destined', b'to', b'be', b'the', b'21st', b'century', b"'s", b'new', b'``', b'conan', b"''", b'and', b'that', b'he', b"'s", b'going', b'to', b'make', b'a', b'splash', b'even', b'greater', b'than', b'arnold', b'schwarzenegger', b',', b'jean-claud', b'van', b'damme', b'or', b'steven', b'segal', b'.']
+        sentlengths -- np.array with the length of each sentence
+        cumsentlen -- list with the cumulative sum of sentlengths
+        """
         if hasattr(self, "_sentences") and self._sentences:
             return self._sentences
 
@@ -70,12 +88,18 @@ class StanfordSentiment:
         return self._sentences
 
 
-    def numSentences(self):
-        if hasattr(self, "_numSentences") and self._numSentences:
-            return self._numSentences
+    def num_sentences(self):
+        """
+        Get the number of sentences
+
+        Returns:
+        num_sentences -- number of sentences
+        """
+        if hasattr(self, "_num_sentences") and self._num_sentences:
+            return self._num_sentences
         else:
-            self._numSentences = len(self.sentences())
-            return self._numSentences
+            self._num_sentences = len(self.sentences())
+            return self._num_sentences
 
 
     def allSentences(self):
@@ -115,21 +139,36 @@ class StanfordSentiment:
             return self.getRandomContext(C)
 
 
-    def sent_labels(self):
-        if hasattr(self, "_sent_labels") and self._sent_labels:
-            return self._sent_labels
+    def sentiment_labels(self):
+        """
+        Get values of sentiment for each label
+
+        Returns:
+        sentiment_labels -- a list with sentiment values assigned to each sentence
+        """
+        if hasattr(self, "_sentiment_labels") and self._sentiment_labels:
+            return self._sentiment_labels
 
         dictionary = dict()
         phrases = 0
+
+        # dictionary.txt contains all phrases and their IDs, separated by a vertical line
+        # Example: ! Brilliant !|40532
+
         with open(self.path + "/dictionary.txt", "r") as f:
             for line in f:
                 line = line.strip()
                 if not line: continue
                 splitted = line.split("|")
+                # An item of dictionary has the form of ('a markedly inactive film , city is conversational bordering on confessional .', 103692)
                 dictionary[splitted[0].lower()] = int(splitted[1])
                 phrases += 1
 
+        # Create a list of 0.0 elements of length equals to the number of sentences
         labels = [0.0] * phrases
+
+        # sentiment_labels.txt contains all phrase ids and the corresponding sentiment labels, separated by a vertical line
+        # Example: 12|0.33333
         with open(self.path + "/sentiment_labels.txt", "r") as f:
             first = True
             for line in f:
@@ -140,28 +179,43 @@ class StanfordSentiment:
                 line = line.strip()
                 if not line: continue
                 splitted = line.split("|")
+                # For each sentence in dictiorary you have an element in the labels list with the value of the sentiment converted in float
                 labels[int(splitted[0])] = float(splitted[1])
 
-        sent_labels = [0.0] * self.numSentences()
+        sentiment_labels = [0.0] * self.num_sentences()
         sentences = self.sentences()
-        for i in range(self.numSentences()):
+
+        for i in range(self.num_sentences()):
             sentence = sentences[i]
+            # XXX Some problem with utf-8 in Python 3
             full_sent = b" ".join(sentence).decode("latin-1").replace('-lrb-', '(').replace('-rrb-', ')').replace('ã©','é')
 
+            # Added a try/catch because sometimes I can't get sentences as key for codification problems
             try:
-                sent_labels[i] = labels[dictionary[full_sent]]
+                # sentiment labels is a list in which the index correspond to an a element of dictionary (a sentence) and the value of the element is the sentiment value for the specific sentence
+                # Remember that with dictionary[full_sent] you get the ID of the sentence
+                # With labels[dictionary[full_sent]] you get the sentiment value assigned to that sentence
+                sentiment_labels[i] = labels[dictionary[full_sent]]
             except KeyError:
                  print("I got a KeyError - reason '%s'" % str(full_sent))
 
-        self._sent_labels = sent_labels
-        return self._sent_labels
+        self._sentiment_labels = sentiment_labels
+        return self._sentiment_labels
 
 
     def dataset_split(self):
+        """
+        It splits the dataset in which the sentences are divided for training, testing, and developing
+
+        Returns:
+        split -- list of 3 elements. For each element you have a list of the indexes of sentences chosen for training, testing and developing
+        """
         if hasattr(self, "_split") and self._split:
             return self._split
 
         split = [[] for i in range(3)]
+        # dataset_split contains the sentence index (corresponding to the index in datasetSentences.txt file) followed by the set label separated by a comma
+        # 1 = train, 2 = test, 3 = dev. Example: 1,1
         with open(self.path + "/dataset_split.txt", "r") as f:
             first = True
             for line in f:
@@ -179,7 +233,7 @@ class StanfordSentiment:
     def getRandomTrainSentence(self):
         split = self.dataset_split()
         sentId = split[0][random.randint(0, len(split[0]) - 1)]
-        return self.sentences()[sentId], self.categorify(self.sent_labels()[sentId])
+        return self.sentences()[sentId], self.categorify(self.sentiment_labels()[sentId])
 
 
     def categorify(self, label):
@@ -209,7 +263,7 @@ class StanfordSentiment:
 
     def getSplitSentences(self, split=0):
         ds_split = self.dataset_split()
-        return [(self.sentences()[i], self.categorify(self.sent_labels()[i])) for i in ds_split[split]]
+        return [(self.sentences()[i], self.categorify(self.sentiment_labels()[i])) for i in ds_split[split]]
 
 
     def sampleTable(self):
